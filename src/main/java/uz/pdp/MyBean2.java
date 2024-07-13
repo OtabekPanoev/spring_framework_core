053b55ada2f32743bd8da105785da34010d24c82
otabekpanoev01 <otabekpanoev01@gmail.com>
2024-07-13 12:42:40 +0500

    initial commit

--- src/main/java/uz/pdp/MyBean2.java
+++ src/main/java/uz/pdp/MyBean2.java
@@ -8,6 +8,14 @@ public class MyBean2 {
 
     private MyBean myBean;
 
+    public MyBean2() {
+    }
+
+    @Autowired
+    public MyBean2(MyBean myBean, String str) {
+        this.myBean = myBean;
+    }
+
     public void hi() {
         System.out.println("hello from MyBean2");
         System.out.println(myBean);
--- src/main/java/uz/pdp/MyConfig.java
+++ src/main/java/uz/pdp/MyConfig.java
@@ -1,7 +1,11 @@
 package uz.pdp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @ComponentScan()
+@Configuration
 public class MyConfig {
+
 }
